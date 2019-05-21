fd443002ea6db1ba9fed81f2505264f2566a3ca1
BancCoin <banccoin@gmail.com>
2019-05-21 09:25:37 +0530

    Update checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,10 +24,19 @@ namespace Checkpoints
     //   (no blocks before with a timestamp after, none after with
     //    timestamp before)
     // + Contains no strange transactions
-    //
+    // 
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         ( 0,   uint256("0x00000da9f638b705f00f8f6fc533aeed36e656996bf31ffc52d56dfdd6b25b5c") ) 
+        ( 100,   uint256("0xa8bf6b2ef5e0d56f991681ad3af689ca230243e20ad3200011c23b498082d2df") ) 
+        ( 510,   uint256("0xce18f2c1153e2c030618a05a1bee8e7dcead88f8b54b20521f69ddcdaed356d2") ) 
+        ( 1010,   uint256("0x2f84b7589d9eb1eb0b167e57235f828b89a21186e8b6e110c17982bb993df5b3") ) 
+        ( 5050,   uint256("0x72ea7a1164cefcf41c10bdb70323ba051e67b33de8be536879930e346bbc18bf") ) 
+        ( 10010,   uint256("0xe3d5cb1ec5b3642e1484deca9719773b5474ad2503533bb4da5adbe461eb8aa0") ) 
+        ( 51000,   uint256("0x5d4e4673ae13e9e0d333a00640d6962fc35a8fb56f166230aa01c1315da67955") ) 
+        ( 81000,   uint256("0xac0d92378b7ac09e1814a016a9d1f64a846fd6b4c7fb9f158b00c32b950537f5") ) 
+        ( 91000,   uint256("0x5beef44f6f5d0de6e9f5fda718c59b166c628ba16663ff5ed8cb34398a86119d") ) 
+        ( 101000,   uint256("0xd7d4aa70b5144e133b0257660515a859e48e557306b3aa30465809ed5cc2e755") ) 
     ;
 
     // TestNet has no checkpoints
